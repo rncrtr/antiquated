8a74564712fbb25c190c55513a95de2c2a6c841e
Ryan Carter <ryan@hotelengine.com>
2014-12-08 10:17:29 -0700

    init, border with class

---
+++ antiquated.js
@@ -0,0 +1,10 @@
+// load jQuery if not already
+window.jQuery || document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"><\/script>');
+
+$(document).ready(function(){
+  $('*').mouseover(function(e){
+    e.css('border','1px solid blue');
+    var info += e.attr('class'); 
+    e.prepend('<span>'+info+'</span>');
+  });
+});
