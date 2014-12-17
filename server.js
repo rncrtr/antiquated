d6ed32d906a17e9f37be1e79cf1a0e284b9a6005
Ryan Carter <ryan@hotelengine.com>
2014-12-17 16:39:51 -0700

    added nodebot server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-/*  First install npm then run this in terminal (without the $): 
+/*  NODEBOT IS YOUR TINY FRIENDLY SERVER. DO NOT BE ALARMED. :)
+*   
+*   1. If you have node/npm, install connect and serve-static in terminal (without the $): 
 *     $ npm install connect serve-static
 *
-*   Then start the server in terminal (without the $): 
+*   2. Start nodebot in terminal (without the $): 
 *     $ node server
 *
 *   Note: Shutdown with Ctrl + C
@@ -10,5 +12,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = 8080;
 connect().use(serveStatic(__dirname)).listen(port);
-
-console.log('Nodebot is serving your site at http://locahost:'+port+'.\r\nYou can change the port in server.js or shutdown with Ctrl+C');
+console.log('Nodebot is now serving your files at http://locahost:'+port+'.\r\nShutdown with Ctrl+C');
